b2e424ee40c907754421678ebcd2245944f97b17
magical01 <denis.verzakov.015@gmail.com>
2022-12-08 22:47:51 +0500

    feat: add adaptive

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,14 +4,40 @@ Swiper.use([Navigation, Scrollbar]);
 
 const swiper = new Swiper('.swiper', {
   loop: false,
-  slidesPerView: '5',
-  spaceBetween: 38,
+  slidesPerView: 5,
+  spaceBetween: 25,
   scrollbar: {
     el: '.slider-scroll',
     draggable: true,
   },
+  speed: 500,
   navigation: {
     nextEl: '.slider-btn-next',
     prevEl: '.slider-btn-prev'
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    528: {
+      slidesPerView: 2,
+      spaceBetween: 15
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+    1198: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  }
 });
